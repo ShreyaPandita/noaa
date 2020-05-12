337686ccc0133daf249cceadceeedf9054835d9c
ShreyaPandita <shreya.pandita@gmail.com>
2020-05-12 18:32:49 +0000

    updated install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt-get install python-pip
+sudo pip install numpy
+sudo pip install pyresample
+pythonn -m pip install -I google-api-python-client
+sudo pip install netcdf4
+sudo pip install matplotlib
+sudo apt-get install python-tk
+sudo pip install pillow
+sudo pip install google-cloud-dataflow
+Sudo pip install google-cloud-storage
